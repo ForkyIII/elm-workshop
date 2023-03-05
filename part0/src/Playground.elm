e2529719911c4827632e714d95b4ef1080fb8178
OmarRamoun <omar@nurserystory.co.uk>
2023-03-05 21:22:27 +0200

    feat: add case exp

--- part0/src/Playground.elm
+++ part0/src/Playground.elm
@@ -87,29 +87,16 @@ displayDegree marks =
 --}
 
 weekday dayInNumber =
-    if dayInNumber == 0 then
-        "Sunday"
+    case dayInNumber of
+        0 -> "Sunday"
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        4 -> "Thursday"
+        5 -> "Friday"
+        6 -> "Saturday"
+        _ -> "Invalid day"
 
-    else if dayInNumber == 1 then
-        "Monday"
-
-    else if dayInNumber == 2 then
-        "Tuesday"
-
-    else if dayInNumber == 3 then
-        "Wednesday"
-
-    else if dayInNumber == 4 then
-        "Thursday"
-
-    else if dayInNumber == 5 then
-        "Friday"
-
-    else if dayInNumber == 6 then
-        "Saturday"
-
-    else
-        "Unknown day"
 
 main =
     Html.h1
