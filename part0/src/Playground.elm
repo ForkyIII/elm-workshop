9b2c79be7e2bfdfe198d7a5b77b1f2dfeb8a99f4
OmarRamoun <omar@nurserystory.co.uk>
2023-03-06 22:26:39 +0200

    feat: regext with options

--- part0/src/Playground.elm
+++ part0/src/Playground.elm
@@ -7,12 +7,15 @@ import Regex
 
 pattern : String
 pattern =
-    "\\d\\d:\\d\\d"
+    "phoenix"
+
+options : Regex.Options
+options = { caseInsensitive = True, multiline = False }
 
 
 maybeRegex : Maybe Regex.Regex
 maybeRegex =
-    Regex.fromString pattern
+    Regex.fromStringWith options pattern
 
 
 regex : Regex.Regex
@@ -23,7 +26,7 @@ regex =
 apollo11 : String
 apollo11 =
     """ 
-    On July 16, 1969, the massive Saturn V rocket lifted
+    On July 16, 1969, the massive Saturn V rocket lifted the PHOenix
 off from NASA's Kennedy Space Center at 09:32 a.m. EDT. Four days later, on July 20 at
     """
 
