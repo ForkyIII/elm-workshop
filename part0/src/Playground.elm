a10f7a8d2d21016e5bd23a3e1896fc46cab2d88a
OmarRamoun <omar@nurserystory.co.uk>
2023-03-07 14:40:19 +0200

    feat: descending order sorting

--- part0/src/Playground.elm
+++ part0/src/Playground.elm
@@ -1,42 +1,25 @@
 module Playground exposing (main)
 
 import Html
-import Html.Attributes exposing (class)
-import Regex
+-- import Html.Attributes exposing (class)
 
 
-pattern : String
-pattern =
-    "phoenix"
+descending2 : comparable -> comparable -> Order
+descending2 a b =
+    case compare a b of
+        LT ->
+            GT
 
-options : Regex.Options
-options = { caseInsensitive = True, multiline = False }
+        GT ->
+            LT
 
-
-maybeRegex : Maybe Regex.Regex
-maybeRegex =
-    Regex.fromStringWith options pattern
-
-
-regex : Regex.Regex
-regex =
-    Maybe.withDefault Regex.never maybeRegex
-
-
-apollo11 : String
-apollo11 =
-    """ 
-    On July 16, 1969, the massive Saturn V rocket lifted the PHOenix
-off from NASA's Kennedy Space Center at 09:32 a.m. EDT. Four days later, on July 20 at
-    """
-
-fromListToString : List String -> String
-fromListToString = 
-    String.join ""
+        EQ ->
+            EQ
 
 
 main : Html.Html msg
 main =
-    Html.h1
-        [ class "h1" ]
-        [ Html.text <| fromListToString <| Regex.split regex apollo11 ]
+    [ 316, 320, 312, 370, 337, 318, 314 ]
+        |> List.sortWith descending2
+        |> Debug.toString
+        |> Html.text
