d71475ba31c24b208621ea0a407903482d9d0878
OmarRamoun <omar@nurserystory.co.uk>
2023-03-07 19:16:29 +0200

    feat: email validator

--- part0/src/Playground.elm
+++ part0/src/Playground.elm
@@ -1,18 +1,37 @@
 module Playground exposing (main)
 
 import Html
--- import Html.Attributes exposing (class)
+import Regex
 
-guardians : List String
-guardians = [ "Star-lord", "Groot", "Gamora", "Drax", "Rocket" ]
 
--- List.foldl (\x a -> (String.length x) + a) 0 guardians
+validate : String -> ( String, String )
+validate email =
+    let
+        pattern : String
+        pattern =
+            "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$"
+
+        maybeRegex : Maybe Regex.Regex
+        maybeRegex =
+            Regex.fromString pattern
+
+        regex : Regex.Regex
+        regex =
+            Maybe.withDefault Regex.never maybeRegex
+
+        isValid : Bool
+        isValid =
+            Regex.contains regex email
+    in
+    if isValid then
+        ( "Valid email", "green" )
+
+    else
+        ( "Invalid email", "red" )
 
 
 main : Html.Html msg
 main =
-    guardians
-        |> List.map String.length
-        |> List.sum
+    validate "omar@nurserym"
         |> Debug.toString
         |> Html.text
